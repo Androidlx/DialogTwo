cd23865eb8a0ca41e3377cc0f87a8ba00b1383e4
Androidlx <1748054721@qq.com>
2017-10-17 20:52:28 +0800

    first commit

---
+++ app/src/main/java/com/example/lixin/lixin/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.lixin.lixin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            showDialog();
+        }
+
+    public static void showDialog() {
+
+    }
+}
