dcc76a40c8868c9179d57d72903994a8ace23d4c
Androidlx <1748054721@qq.com>
2017-10-18 13:05:50 +0800

    shengji

--- app/src/main/java/com/example/lixin/lixin/MainActivity.java
+++ app/src/main/java/com/example/lixin/lixin/MainActivity.java
@@ -1,12 +1,30 @@
 package com.example.lixin.lixin;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
 
 public class MainActivity{
 
 
-    public static void showDialog() {
-
+    public static void showDialog(Activity activity) {
+        new SweetAlertDialog(activity, SweetAlertDialog.WARNING_TYPE)
+                .setTitleText("Are you sure?")
+                .setContentText("Won't be able to recover this file!")
+                .setConfirmText("Yes,delete it!")
+                .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                    @Override
+                    public void onClick(SweetAlertDialog sDialog) {
+                        sDialog
+                                .setTitleText("Deleted!")
+                                .setContentText("Your imaginary file has been deleted!")
+                                .setConfirmText("OK")
+                                .setConfirmClickListener(null)
+                                .changeAlertType(SweetAlertDialog.SUCCESS_TYPE);
+                    }
+                })
+                .show();
     }
 }
